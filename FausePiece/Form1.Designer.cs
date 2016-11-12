e97d48589feb8d7f7aaac987a550da447641a332
Виктор <@ACER>
2016-11-12 14:26:46 +0100

    Add project files.

---
+++ FausePiece/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace FausePiece
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.Answer = new System.Windows.Forms.TextBox();
+            this.LocalMax = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numberOfBags = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.maxPieces = new System.Windows.Forms.NumericUpDown();
+            this.Log = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numberOfBags)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.maxPieces)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(392, 265);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Answer
+            // 
+            this.Answer.Location = new System.Drawing.Point(29, 154);
+            this.Answer.Name = "Answer";
+            this.Answer.Size = new System.Drawing.Size(383, 20);
+            this.Answer.TabIndex = 1;
+            // 
+            // LocalMax
+            // 
+            this.LocalMax.AutoSize = true;
+            this.LocalMax.Location = new System.Drawing.Point(432, 161);
+            this.LocalMax.Name = "LocalMax";
+            this.LocalMax.Size = new System.Drawing.Size(35, 13);
+            this.LocalMax.TabIndex = 2;
+            this.LocalMax.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Number of bags";
+            // 
+            // numberOfBags
+            // 
+            this.numberOfBags.Location = new System.Drawing.Point(121, 13);
+            this.numberOfBags.Name = "numberOfBags";
+            this.numberOfBags.Size = new System.Drawing.Size(46, 20);
+            this.numberOfBags.TabIndex = 4;
+            this.numberOfBags.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Maximum pieces";
+            // 
+            // maxPieces
+            // 
+            this.maxPieces.Location = new System.Drawing.Point(121, 40);
+            this.maxPieces.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.maxPieces.Name = "maxPieces";
+            this.maxPieces.Size = new System.Drawing.Size(43, 20);
+            this.maxPieces.TabIndex = 6;
+            this.maxPieces.Value = new decimal(new int[] {
+            6,
+            0,
+            0,
+            0});
+            // 
+            // Log
+            // 
+            this.Log.Location = new System.Drawing.Point(487, 13);
+            this.Log.Name = "Log";
+            this.Log.Size = new System.Drawing.Size(270, 303);
+            this.Log.TabIndex = 7;
+            this.Log.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(760, 328);
+            this.Controls.Add(this.Log);
+            this.Controls.Add(this.maxPieces);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numberOfBags);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LocalMax);
+            this.Controls.Add(this.Answer);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numberOfBags)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.maxPieces)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox Answer;
+        private System.Windows.Forms.Label LocalMax;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numberOfBags;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown maxPieces;
+        private System.Windows.Forms.RichTextBox Log;
+    }
+}
+
---
+++ FausePiece/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace FausePiece
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var tekBags = new int[(int) numberOfBags.Value];
+            var prevState = new[] {1, 2, 3, 4, 5, 39, 97, 139, 247, 478};
+            if ((int) numberOfBags.Value != 10)
+                for (var i = 0; i < (int) numberOfBags.Value; i++)
+                    tekBags[i] = i + 1;
+            else
+                tekBags = prevState;
+            var localMaximum = (int) maxPieces.Value + 1;
+            var ourBags = new Bags((int) numberOfBags.Value);
+
+            while (tekBags[0] != localMaximum - (int) numberOfBags.Value)
+            {
+                ourBags.SetBags(tekBags);
+
+                if (ourBags.BagsUniqTest() && (ourBags.MaxBagValue() < localMaximum))
+                {
+                    Log.Text += ourBags.GetBagsAsString() + @";
+";
+                    localMaximum = ourBags.MaxBagValue();
+                    Answer.Text = ourBags.GetBagsAsString();
+                    Refresh();
+                }
+
+                if (tekBags[(int) numberOfBags.Value - 1] >= localMaximum)
+                {
+                    var i = (int) numberOfBags.Value - 1;
+                    while ((i > 0) && (tekBags[i] - 1 == tekBags[i - 1])) i--;
+                    tekBags[i - 1]++;
+                    for (var j = i; j < (int) numberOfBags.Value; j++) tekBags[j] = tekBags[j - 1] + 1;
+                }
+                else
+                {
+                    tekBags[(int) numberOfBags.Value - 1]++;
+                }
+            }
+            LocalMax.Text = localMaximum.ToString();
+        }
+    }
+
+    public class Bags
+    {
+        private int[] _bagsValue;
+        private int[] _weightValue;
+
+        public Bags(int size)
+        {
+            _bagsValue = new int[size];
+            for (var i = 0; i < size; i++)
+                _bagsValue[i] = 1;
+        }
+
+        public Bags(int[] arr)
+        {
+            _bagsValue = arr;
+        }
+
+        public void SetBags(int[] arr)
+        {
+            _bagsValue = arr;
+        }
+
+        public int[] GetBags()
+        {
+            return _bagsValue;
+        }
+
+        public string GetBagsAsString()
+        {
+            return _bagsValue.Aggregate("", (current, i) => current + (i + " "));
+        }
+
+        public int MaxBagValue()
+        {
+            return _bagsValue.Max();
+        }
+
+        public bool BagsUniqTest()
+        {
+            for (var i = 0; i < _bagsValue.Length - 2; i++)
+                for (var j = 1; i < _bagsValue.Length - 1; i++)
+                    for (var k = 2; i < _bagsValue.Length; i++)
+                        if (_bagsValue[i] + _bagsValue[j] == _bagsValue[k])
+                            return false;
+
+
+            _weightValue = new int[(int) Math.Pow(2, _bagsValue.Length)];
+            for (var i = 1; i < _weightValue.Length; i++)
+            {
+                var currNumber = _bagsValue.Select((t, j) => t*((i >> j)%2)).Sum();
+                if (Array.IndexOf(_weightValue, currNumber) == -1)
+                    _weightValue[i] = currNumber;
+                else
+                    return false;
+            }
+            return true;
+        }
+    }
+}
